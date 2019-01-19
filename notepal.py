2785002da637770f75bd7c31716d6f1133bba1ea
UTkzhang <kevinhk.zhang@mail.utoronto.ca>
2019-01-19 13:18:41 -0500

    Added file management and integration with image/audio capture

--- notepal.py
+++ notepal.py
@@ -5,9 +5,15 @@ import cv2
 import time
 import requests
 import json
+import os
+import subprocess
+import signal
 
 currPage = 1
 currName = "Default"
+currProc = None
+currStatus = "READY"
+currDir = None
 
 #POST function
 def post(r):
@@ -29,29 +35,87 @@ def default():
 @assist.action("startNote", mapping={'name': 'name'})
 def startNote(name):
 	global currName
+	global currProc
+	global currStatus
+	global currDir
+	
+	if currStatus != "READY":
+		return tell("Your note " + str(currName) + " has already been started!")
+	
+	currStatus = "STARTED"
 	currName = name
-	#start recording
+	filepath = "Notes/" + currName
+	
+	counter = 1
+	while os.path.exists("%s/Note %d"%(filepath,counter)):
+		counter += 1
+	directory = "%s/Note %d"%(filepath,counter)
+
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	currDir = directory
+
+	recordName = os.path.join(directory,"record 1.wav")
+	proc_args = ['arecord', '-D' , 'plughw:1,0' , '-c 2' , '-r' , '16' , '-f' , 'S16_LE' , recordName]
+	rec_proc = subprocess.Popen(proc_args, shell=False, preexec_fn=os.setsid)
+	print("startRecordingArecord()> rec_proc pid= " + str(rec_proc.pid))
+	print("startRecordingArecord()> recording started")
+	currProc = rec_proc
+	
 	return tell("Your note " + str(currName) + " has been started.")
 	
 @assist.action("newPage")
 def newPage():
 	global currPage
-	#capture photo, stop recording, start new recording
+	global currProc
+	global currStatus
+	global currDir
+	
+	if currStatus != "STARTED":
+		return tell("OK, I know you weren't the brightest, but I was hoping you would know to start a new note before turning to the next page.")
+
+	captureImage("%s/image %d"%(currDir,currPage))
+	os.killpg(currProc.pid, signal.SIGTERM)
+	currProc.terminate()
+	currProc = None
+	print("stopRecordingArecord()> Recording stopped")
 	currPage += 1
+	recordName = os.path.join(currDir,"record %d.wav"%(currPage))
+	proc_args = ['arecord', '-D' , 'plughw:1,0' , '-c 2' , '-r' , '16' , '-f' , 'S16_LE' , recordName]
+	rec_proc = subprocess.Popen(proc_args, shell=False, preexec_fn=os.setsid)
+	print("startRecordingArecord()> rec_proc pid= " + str(rec_proc.pid))
+	print("startRecordingArecord()> recording started")
+	currProc = rec_proc
 	return tell("Ok, you are now on page " + str(currPage) + ".")
 
 @assist.action("endNote")
 def endNote():
 	global currName
 	global currPage
+	global currProc
+	global currStatus
+	global currDir
+
+	if currStatus != "STARTED":
+		return tell("OK, I know you weren't the brightest, but I was hoping you would know to start a new note before ending it.")
+	
+	captureImage("%s/image %d"%(currDir,currPage))
+	os.killpg(currProc.pid, signal.SIGTERM)
+	currProc.terminate()
+	currProc = None
+	print("stopRecordingArecord()> Recording stopped")
+	
 	temp = currPage
 	currPage = 1
-	#capture photo, stop recording
+	currStatus = "READY"
+	currDir = None
 	#call speech to text APIs, upload text and images
 	return tell("Ok, your note " + currName + " with " + str(temp) + " pages is now completed. It has been saved automatically and should be viewable in your account in a few minutes!")
 
 
 if __name__ == "__main__":
+	if not os.path.exists("Notes"):
+		os.makedirs("Notes")
 	app.run(debug=True)
 
 
