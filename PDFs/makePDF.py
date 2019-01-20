0b590d746a89f060d6f9f1d665ef7128a5b2bf83
blitzingeagle <mcmorrischen@gmail.com>
2019-01-19 20:57:14 -0500

    Almost fixed PDF

--- PDFs/makePDF.py
+++ PDFs/makePDF.py
@@ -2,26 +2,18 @@ from pylatex import Document, Figure, Section, Subsection, Command
 from pylatex.utils import italic, NoEscape
 import os
 
-
 def fill_page(doc, section, text, image, image_num):
-    """Add a section, a subsection and some text to the document.
+	print("DEBUG: " + text)
+	f = open(text, "r")
+	text = f.read()
 
-    :param doc: the document
-    :type doc: :class:`pylatex.document.Document` instance
-    """
-    print("DEBUG: " + text)
-    f = open(text, "r")
-    text = f.read()
-    
-    image_abspath = "image 1.jpg"
-  
-    print("DEBUG: " + image_abspath)
-    with doc.create(Section(section)):
+	print("DEBUG: " + image)
+	with doc.create(Section(section)):
 		with doc.create(Subsection("Page " + str(image_num))):
 			with doc.create(Figure(position='h!')) as img:
-				img.add_image(image_abspath, width='300px')
-                img.add_caption("Image " + str(image_num))    
-		doc.append(text)
+				img.add_image(image, width='300px')
+				img.add_caption("Image " + str(image_num))    
+			doc.append(text)
 	
 def makePDF(input_dir, course):
 	doc = Document(course)
@@ -38,8 +30,8 @@ def makePDF(input_dir, course):
 				fill_page(doc, dir, os.path.join(os.path.join(input_dir, dir), "record " + str(counter) + ".wav.txt"), os.path.join(os.path.join(input_dir, dir), "image " + str(counter) + ".jpg"), counter)
 				counter += 1
 		counter = 1
-	doc.generate_pdf(os.path.join("PDFs", course), clean_tex=False)
+	doc.generate_pdf(course, clean = True, clean_tex=False)
     
 if __name__ == "__main__":
-	makePDF("Notes/Algorithms and Data Structures", "Algorithms and Data Structures")
+	makePDF("../Notes/Algorithms and Data Structures", "Algorithms and Data Structures")
 
